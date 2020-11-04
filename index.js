0bbb79cc72eab24119a5348d2f36714b7911c215
tidiview <francois.vidit@laposte.net>
2020-11-04 13:09:09 +0100

    chage to tylergaw version

--- index.js
+++ index.js
@@ -1,112 +1,70 @@
-require('dotenv').config({silent: true})
-const express = require('express')
-const simpleOauthModule = require('simple-oauth2')
-const randomstring = require('randomstring')
-const port = process.env.PORT || 3000
-const oauthProvider = process.env.OAUTH_PROVIDER || 'github'
-const loginAuthTarget = process.env.AUTH_TARGET || '_self'
+const express = require("express");
+const tiny = require("tiny-json-http");
 
-const app = express()
-const oauth2 = simpleOauthModule.create({
-  client: {
-    id: process.env.OAUTH_CLIENT_ID,
-    secret: process.env.OAUTH_CLIENT_SECRET
-  },
-  auth: {
-    // Supply GIT_HOSTNAME for enterprise github installs.
-    tokenHost: process.env.GIT_HOSTNAME || 'https://github.com',
-    tokenPath: process.env.OAUTH_TOKEN_PATH || '/login/oauth/access_token',
-    authorizePath: process.env.OAUTH_AUTHORIZE_PATH || '/login/oauth/authorize'
-  }
-})
+const client_id = process.env.OAUTH_CLIENT_ID;
+const client_secret = process.env.OAUTH_CLIENT_SECRET;
+const authUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,user`;
+const tokenUrl = "https://github.com/login/oauth/access_token";
 
-const originPattern = process.env.ORIGIN || ''
-if (('').match(originPattern)) {
-  console.warn('Insecure ORIGIN pattern used. This can give unauthorized users access to your repository.')
-  if (process.env.NODE_ENV === 'production') {
-    console.error('Will not run without a safe ORIGIN pattern in production.')
-    process.exit()
-  }
-}
+const app = express();
 
-// Authorization uri definition
-const authorizationUri = oauth2.authorizationCode.authorizeURL({
-  redirect_uri: process.env.REDIRECT_URL,
-  scope: process.env.SCOPES || 'repo,user',
-  state: randomstring.generate(32)
-})
+// NetlifyCMS doesn't use this root page. It's only for dev purposes
+app.get("/", (req, res) => {
+  res.send(`<a href="${authUrl}">Login with Github</a>`);
+});
 
-// Initial page redirecting to Github
-app.get('/auth', (req, res) => {
-  res.redirect(authorizationUri)
-})
+// NetlifyCMS expects to land on a page at /auth.
+app.get("/auth", (req, res) => res.redirect(authUrl));
 
-// Callback service parsing the authorization token and asking for the access token
-app.get('/callback', (req, res) => {
-  const code = req.query.code
-  var options = {
-    code: code
-  }
-
-  if (oauthProvider === 'gitlab') {
-    options.client_id = process.env.OAUTH_CLIENT_ID
-    options.client_secret = process.env.OAUTH_CLIENT_SECRET
-    options.grant_type = 'authorization_code'
-    options.redirect_uri = process.env.REDIRECT_URL
-  }
-
-  oauth2.authorizationCode.getToken(options, (error, result) => {
-    let mess, content
-
-    if (error) {
-      console.error('Access Token Error', error.message)
-      mess = 'error'
-      content = JSON.stringify(error)
-    } else {
-      const token = oauth2.accessToken.create(result)
-      mess = 'success'
-      content = {
-        token: token.token.access_token,
-        provider: oauthProvider
-      }
-    }
-
+app.get("/callback", async (req, res) => {
+  const data = {
+    code: req.query.code,
+    client_id,
+    client_secret
+  };
+  
+  try {
+    const { body } = await tiny.post({ 
+      url: tokenUrl, 
+      data, 
+      headers: {
+        // GitHub returns a string by default, ask for JSON to make the reponse easier to parse.
+        "Accept": "application/json" 
+      } 
+    });
+    
+    const postMsgContent = {
+      token: body.access_token,
+      provider: "github"
+    };
+    
+    // This is what talks to the NetlifyCMS page. Using window.postMessage we give it the
+    // token details in a format it's expecting
     const script = `
     <script>
     (function() {
       function recieveMessage(e) {
-        console.log("recieveMessage %o", e)
-        if (!e.origin.match(${JSON.stringify(originPattern)})) {
-          console.log('Invalid origin: %s', e.origin);
-          return;
-        }
-        // send message to main window with da app
+        console.log("recieveMessage %o", e);
+        
+        // send message to main window with the app
         window.opener.postMessage(
-          'authorization:${oauthProvider}:${mess}:${JSON.stringify(content)}',
+          'authorization:github:success:${JSON.stringify(postMsgContent)}', 
           e.origin
-        )
+        );
       }
-      window.addEventListener("message", recieveMessage, false)
-      // Start handshare with parent
-      console.log("Sending message: %o", "${oauthProvider}")
-      window.opener.postMessage("authorizing:${oauthProvider}", "*")
+
+      window.addEventListener("message", recieveMessage, false);
+      window.opener.postMessage("authorizing:github", "*");
     })()
-    </script>`
-    return res.send(script)
-  })
-})
+    </script>`;
+    
+    return res.send(script);
+    
+  } catch (err) {
+    // If we hit an error we'll handle that here
+    console.log(err);
+    res.redirect("/?error=😡");
+  }
+});
 
-app.get('/success', (req, res) => {
-  res.send('')
-})
-
-app.get('/', (req, res) => {
-  res.send(`Hello<br>
-    <a href="/auth" target="${loginAuthTarget}">
-      Log in with ${oauthProvider.toUpperCase()}
-    </a>`)
-})
-
-app.listen(port, () => {
-  console.log("gandalf is walkin' on port " + port)
-})
+app.listen(process.env.PORT);
